eef64e1d04c637ccf566fba96f857d6b0d39eed1
Magdalena Skarbinska <mskarbinska@google.com>
2025-06-05 10:37:57 +0000

    add vitest and prettier

--- packages/sso-offloading-connector/vite.config.ts
+++ packages/sso-offloading-connector/vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/sso_offloading_connector.ts'),
       name: 'SsoOffloadingConnector',
       fileName: 'sso-offloading-connector',
     },
